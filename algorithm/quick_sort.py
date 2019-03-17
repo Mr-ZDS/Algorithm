822311366a67121c1baf135531b587673700a90b
张艾迪 <zhdashuai@foxmail.com>
2019-03-17 16:03:35 +0800

    commit algorithm file

--- algorithm/quick_sort.py
+++ algorithm/quick_sort.py
@@ -1,10 +1,11 @@
+#算法导论快排：已最右边元素为基准元素x开始，比x小的放在x左边，大的放右边
 def quick_sort(array, l, r):
     if l < r:
         visit = partition(array, l, r)    #记录每趟排序后基准元素的位置
         quick_sort(array, l, visit - 1)
         quick_sort(array, visit + 1, r)
 
-def partition(array, l, r):
+def partition(array, l, r):         #此函数为单向扫描法
     x = array[r]
     i = l-1
     for j in range(l, r):
@@ -17,4 +18,5 @@ def partition(array, l, r):
 list = [1,4,8,33,63,7,5,2,8,5]
 quick_sort(list, 0, len(list)-1)
 print(list)
+
 
