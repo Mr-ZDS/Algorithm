81cf4b8adb5f17e32975333b7918120062c86b30
张艾迪 <zhdashuai@foxmail.com>
2019-03-17 20:04:27 +0800

    commit algorithm file

---
+++ algorithm/binary_search.py
@@ -0,0 +1,23 @@
+#有序表的二分查找
+import numpy
+def Binary_search(list, value):
+    low = 0
+    high = len(list)
+    while low < high:
+        mid = int((low + high) / 2)
+        if list[mid] > value:
+            high = mid - 1
+        elif list[mid] < value:
+            low = mid + 1
+        else:
+            print(mid)
+            return mid
+    print('无法找到查找的值！！！')
+    return False
+
+#随机数生成一个列表
+list = numpy.random.randint(2, 20, size = 10)
+list.sort()
+print(list)
+value = numpy.random.randint(20)
+Binary_search(list,value)
