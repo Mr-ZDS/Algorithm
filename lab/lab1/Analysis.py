1b82cfab30e4d3d524e3a5192adbed4673ae3b33
张艾迪 <zhdashuai@foxmail.com>
2019-03-27 20:43:35 +0800

    commit -m file

--- lab/lab1/Analysis.py
+++ lab/lab1/Analysis.py
@@ -1,5 +1,6 @@
 class analysis:
-    list=[]
+    list1=[]     #记录单词内容
+    list2=[]     #记录单词类别
     Keyword = ['void', 'int', 'char', 'float', 'double', 'bool', 'long', 'short', 'signed', 'unsigned', 'true', 'false',
                'const', 'inline', 'auto', 'static', 'extern', 'register', 'for', 'while', 'if', 'else', 'do', 'switch',
                'case', 'default', 'break', 'continue', 'return', 'goto', 'new', 'delete', 'sizeof', 'class', 'struct',
@@ -14,32 +15,34 @@ class analysis:
     def __init__(self, str):
         self.str = str
 
+    # 判断是否为关键字
     def Iskey(self, str):
         for i in self.Keyword:
             if str == i:
                 return True
 
+    # 判断是否单特殊符号
     def Is1(self, str):
         for i in self.Symbol1:
             if str == i:
                 return True
 
+    # 判断双特殊符号
     def Is2(self, str):
         for i in self.Symbol2:
             if str == i:
                 return True
 
     def Is(self):
-        str = self.str+' '
+        str = self.str + ' '
         length = len(str)
         str_visit = ''
         first = ''
-        visit=''
+        visit = ''
         i = 0
         while i < length:
             ch = str[i]
             i += 1
-
             if str_visit == '':  # 如果存储字符串为空，将首个字符加进存储字符串中
                 str_visit += ch
                 first = ch
@@ -52,37 +55,42 @@ class analysis:
                     str_visit += ch
                     continue
                 elif i == length:
-                    self.list.append(str_visit+ '    数字')
+                    self.list1.append(str_visit)
+                    self.list2.append('数字')
                     break
-
                 else:
-                    self.list.append(str_visit+ '    数字')
+                    self.list1.append(str_visit)
+                    self.list2.append('数字')
                     str_visit = ''
                     first = ''
                     i -= 1
                     continue;
 
-
             elif self.Is1(first):  # 以特殊符号开头
                 if self.Is1(ch):
-                    visit=str_visit+ch
+                    visit = str_visit + ch
                     if self.Is2(visit):
-                        self.list.append(visit+ '    特殊符号')
+                        self.list1.append(visit)
+                        self.list2.append('特殊符号')
                         str_visit = ''
-                        visit=''
+                        visit = ''
                         first = ''
+                    elif visit == '//':  # 如果是单行注释，则直接结束循环
+                        break
                     else:
-                        self.list.append(str_visit+ '    特殊符号')
-                        i-=1
-                        str_visit=''
-                        first=''
+                        self.list1.append(str_visit)
+                        self.list2.append('特殊符号')
+                        i -= 1
+                        str_visit = ''
+                        first = ''
                         continue
 
                 else:
-                    self.list.append(str_visit+'     特殊符号')
+                    self.list1.append(str_visit)
+                    self.list2.append('特殊符号')
                     i -= 1  # 回退一位
                     str_visit = ''
-                    first=''
+                    first = ''
                     continue
 
             elif first.isalpha():  # 判断以字母开头的字符串
@@ -95,18 +103,27 @@ class analysis:
                 elif ch == '_':
                     str_visit += ch
                     continue
-
                 else:
                     if self.Iskey(str_visit):
-                        self.list.append(str_visit+'     关键字')
+                        self.list1.append(str_visit)
+                        self.list2.append('关键字')
                     else:
-                        self.list.append(str_visit+'     标识符')
+                        self.list1.append(str_visit)
+                        self.list2.append('标识符')
                     str_visit = ''
                     first = ''
                     i -= 1
 
+            elif first == '"':
+                if ch == '"':
+                    str_visit += ch
+                    self.list1.append(str_visit)
+                    self.list2.append('串')
+                    str_visit = ''
+                    first = ''
+                else:
+                    str_visit += ch
+                continue
+
             else:
-                print('Error!!!!')
-
-
-
+                print('Error!!!')
--- lab/lab1/ui.py
+++ lab/lab1/ui.py
@@ -1,70 +1,89 @@
 import tkinter
 from tkinter import *
+from tkinter import ttk
 import tkinter.filedialog
 from Analysis import analysis
+
+
 class GUI():
 
-    def __init__(self,name):
-        self.name=name
+    def __init__(self, name):
+        self.name = name
 
     def creat(self):
         self.name.title('词法分析')
-        self.name.geometry('450x370+350+120')         #设置窗口大小和偏移度
-        self.button1=tkinter.Button(master = self.name,text='选择文件',command=self.choose)
-        self.button1.grid(row=0,column=11)
-        self.button2=tkinter.Button(self.name,text='分析',command=self.tran)
-        self.button2.grid(row=6,column=11)
+        self.name.geometry('450x370+350+120')  # 设置窗口大小和偏移度
+        self.button1 = tkinter.Button(master = self.name, text = '选择文件', command = self.choose)
+        self.button1.grid(row = 0, column = 11)
+        self.button2 = tkinter.Button(self.name, text = '分析', command = self.tran)
+        self.button2.grid(row = 6, column = 11)
         self.button3 = tkinter.Button(self.name, text = '写入', command = self.write)
         self.button3.grid(row = 5, column = 11)
-        self.text=tkinter.Text(master = self.name,width=55,height=1)
-        self.text.grid(row=2,column=7,columnspan=6,rowspan=1)
-        self.text1=tkinter.Text(master = self.name,width=25)
-        self.text1.grid(row=3,column=0,rowspan=7,columnspan=10)
-        self.text2=tkinter.Text(master = self.name,width=25)
-        self.text2.grid(row=3,column=12,rowspan=7,columnspan=10)
+        self.text = tkinter.Text(master = self.name, width = 55, height = 1)
+        self.text.grid(row = 2, column = 7, columnspan = 6, rowspan = 1)
+        self.text1 = tkinter.Text(master = self.name, width = 25)
+        self.text1.grid(row = 3, column = 0, rowspan = 7, columnspan = 10)
+        #表格定义
+        self.tree = ttk.Treeview(master = self.name, columns = ('单词', '类别'), show = 'headings', height = 14)
+        self.tree.column('单词', width = 90)  # 居中加入  anchor='center'
+        self.tree.column('类别', width = 90)
+        self.tree.heading('单词', text = '单词')
+        self.tree.heading('类别', text = '类别')
+        self.tree.grid(row = 3, column = 12, rowspan = 7, columnspan = 10)
 
-    def choose(self):        #打开并选择文件
-        #应该设置再次点击会清空文本框
+    def choose(self):  # 打开并选择文件
+        # 应该设置再次点击会清空文本框
         filenames = tkinter.filedialog.askopenfilenames()
         if len(filenames) != 0:
             filename = ""
             for i in range(0, len(filenames)):
                 filename += str(filenames[i]) + "\n"
-            self.Filename=filename       #存取选择的文件路径
-            self.text.insert(1.0, '文件路径为:'+filename)
+            self.Filename = filename  # 存取选择的文件路径
+            self.text.insert(1.0, '文件路径为:' + filename)
         else:
-            self.label.config(text = "您没有选择任何文件");
+            self.text.insert(1.0, "您没有选择任何文件");
 
     def write(self):
-        f=open(self.Filename.strip('\n'))         #此时的路径结尾带有'\n'换行符，需要去掉
-        line=f.readline()
-        i=1.0
-        while line!='':
-            self.text1.insert(i,line)
-            i+=1.0
-            line=f.readline()
+        self.text1.delete('1.0','end')        #每次写入时先清空文本框
+        f = open(self.Filename.strip('\n'))  # 此时的路径结尾带有'\n'换行符，需要去掉
+        line = f.readline()
+        i = 1.0
+        while line != '':
+            self.text1.insert(i, line)
+            i += 1.0
+            line = f.readline()
         f.close()
 
     def tran(self):
-        f=open(self.Filename.strip('\n'))
-        line=f.readline().strip('\n')
-        while line!='':
-            t1=analysis(line)
+        self.text1.delete('1.0','end')       #点击分析按钮时会清空文本框，之后输入压缩文本
+        self.tree.delete(*self.tree.get_children())      #点击后清空表格内容
+        f = open(self.Filename.strip('\n'))
+        line = f.readline().strip('\n')
+        i = 1  # 记录表格的行数
+        result = ''
+        while line != '':
+            t1 = analysis(line)
             t1.Is()
-            for lines in t1.list:
-                lines+='\n'
-                self.text2.insert(INSERT,lines)
-            t1.list.clear()
+            j = 0  # 记录list2列表的下标
+            for lines in t1.list1:
+                result += lines
+                lines += '\n'
+                self.tree.insert('', i, values = (lines, t1.list2[j]))
+                i += 1
+                j += 1
+            j = 0
+            t1.list1.clear()
+            t1.list2.clear()
             line = f.readline().strip('\n')
         f.close()
-
-
+        self.text1.insert(INSERT, result)
 
 
 def start():
-    gui=tkinter.Tk()
-    windows=GUI(gui)
+    gui = tkinter.Tk()
+    windows = GUI(gui)
     windows.creat()
     gui.mainloop()
+
 
 start()
