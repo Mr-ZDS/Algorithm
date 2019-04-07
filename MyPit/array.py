600a6618d796d648412c07a9cb5c9daff8abdfa1
张艾迪 <zhdashuai@foxmail.com>
2019-04-07 13:47:10 +0800

    commit MyPit file

---
+++ MyPit/array.py
@@ -0,0 +1,16 @@
+#二维数组踩坑
+
+#二维数组初始化
+n=int(input())
+list=[[0 for i in range(n)] for j in range(n)]      #前为列数，后为行数
+print(list) 
+list[0][1]=1
+print(list)
+
+
+#用*初始化二维数组在修改时会出现错误
+n=int(input())
+list1=[[0]*n]*n
+print(list1)
+list1[0][1]=1
+print(list1)
